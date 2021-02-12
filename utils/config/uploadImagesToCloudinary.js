f04f11df0b9372a695e9625cd0d4a1ad24b6860e
Daniel González <d.g.g69@me.com>
2021-02-12 13:38:36 -0600

    refactor(uploadimagestocloudinary): rewrite function to decrease code complexity

--- utils/config/uploadImagesToCloudinary.js
+++ utils/config/uploadImagesToCloudinary.js
@@ -1,8 +1,39 @@
 import fs from 'fs'
 
+const uploadImage = (file) => {
+  const { $cloudinary } = require('@nuxtjs/cloudinary')
+
+  if (fs.lstatSync(file).isDirectory()) {
+    return null
+  }
+
+  const folder = file.substr(0, file.lastIndexOf('/')).replace('static', '')
+
+  return $cloudinary.upload(file, {
+    folder,
+    use_filename: true,
+    unique_filename: false,
+    overwrite: false,
+  })
+}
+
+const logUploadedFiles = (files) => {
+  const newFiles = files.filter((files) => files && !files.existing)
+
+  if (newFiles.length > 0) {
+    // eslint-disable-next-line no-console
+    console.log({ newFiles })
+  }
+  const previouslyUploadedFiles = files.length - newFiles.length
+
+  // eslint-disable-next-line no-console
+  console.log(
+    `Previously uploaded files to Cloudinary: ${previouslyUploadedFiles}`
+  )
+}
+
 const uploadImagesToCloudinary = () => {
   const glob = require('glob')
-  const { $cloudinary } = require('@nuxtjs/cloudinary')
   const assetsFolder = 'static/images'
   const globPattern = `${assetsFolder}/**/*`
   glob(globPattern, async (error, files) => {
@@ -11,28 +42,9 @@ const uploadImagesToCloudinary = () => {
     }
 
     const uploadedFiles = await Promise.all(
-      files.map((file) => {
-        if (fs.lstatSync(file).isDirectory()) {
-          return null
-        }
-
-        const folder = file
-          .substr(0, file.lastIndexOf('/'))
-          .replace('static', '')
-
-        return $cloudinary.upload(file, {
-          folder,
-          use_filename: true,
-          unique_filename: false,
-          overwrite: false,
-        })
-      })
+      files.map((file) => uploadImage(file))
     )
-    const newFiles = uploadedFiles.filter(
-      (uploadedFile) => uploadedFile && !uploadedFile.existing
-    )
-    // eslint-disable-next-line no-console
-    console.log({ newFiles })
+    logUploadedFiles(uploadedFiles)
   })
 }
 
