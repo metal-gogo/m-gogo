b954c7806985a2d2702c6a3bba9b3c08dfe25da3
mgogo <mgogo@hey.com>
2026-03-08 00:19:50 -0600

    fix: upgrade nuxt to 2.18.1 and add postcss 8 to fix Node 18 compatibility

--- nuxt.config.js
+++ nuxt.config.js
@@ -151,11 +151,7 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-    loaders: {
-      scss: { implementation: require('sass') },
-    },
-  },
+  build: {},
 
   hooks: {
     'content:file:beforeInsert': async (document) => {
